bc24903a7e2229a1f67a9abb54d4355e7ada5e32
reithosa <roujoded@gmail.com>
2026-01-29 10:32:27 +0300

    Загрузка labels на изображение готова

--- Backend_modul/v1/static/js/image.js
+++ Backend_modul/v1/static/js/image.js
@@ -14,6 +14,15 @@ document.addEventListener("DOMContentLoaded", () => {
     let shift = {x: 0, y: 0};
     let scale = 1;
     let imageBox = null;
+
+    const rect0 = new Konva.Rect({
+        visible: false,
+        fill: 'transparent',
+        strokeWidth: 1,
+        stroke: 'red',
+        draggable: false,
+        strokeScaleEnabled: false,
+    });
     
     const canvas_div = document.getElementById('canvas_div');
     const stage = new Konva.Stage({
@@ -27,9 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const app = document.getElementById('app'); 
     const img_path = app.dataset.path;
-    // let labels = document.getElementById('{{ image_labels|tojson|safe }}').textContent;
     let labels = JSON.parse(window.labels);
-    console.log(typeof labels);
     
 
     //Изображение с кровью
@@ -37,17 +44,41 @@ document.addEventListener("DOMContentLoaded", () => {
         let image = konvaImage.image();
         let x_position = Math.floor((canvas_div.offsetWidth - image.naturalWidth) / 2);
         let y_position = Math.floor((canvas_div.offsetHeight - image.naturalHeight) / 2);
-
         konvaImage.setAttrs({
             x: x_position,
             y: y_position,
             name: 'image',
             draggable: false
         });
-
         //Функции не работают из-за того, что изображение загрузиться не успевает, поэтому тут задаю объект
         imageBox = konvaImage;
         bloodLayer.add(konvaImage);
+
+        let counts = 1
+        for (const item of labels) {
+            const rect = rect0.clone();
+            
+            let color = '';
+            if (item.cls == 'WBC') {
+                color = 'purple';
+            } else if (item.cls == 'PLT') {
+                color = 'DarkSlateGray';
+            } else {
+                color = 'red';
+            }
+            rect.setAttrs({
+                x: x_position + item.x - (item.w / 2),
+                y: y_position + item.y - (item.h / 2),
+                width: item.w,
+                height: item.h,
+                name: `rect_${counts}`,
+                zindex: counts,
+                stroke: color,
+                visible: true,
+            });
+            counts += 1;
+            bloodLayer.add(rect);
+        };
         bloodLayer.draw();
     });
 
@@ -147,30 +178,4 @@ document.addEventListener("DOMContentLoaded", () => {
     cursor.addEventListener('click', () => {
         tool = 'cursor';
     });
-
-    //Разметка
-
-    /*const rect0 = new Konva.Rect({
-        visible: false,
-        fill: 'transparent',
-        strokeWidth: 1,
-        stroke: 'red',
-        draggable: false,
-        strokeScaleEnabled: false,
-
-        dragBoundFunc: function(pos) {
-            const box = imageBox.getClientRect();
-            const objRect = this.getClientRect();
-
-            pos.x = Math.max(pos.x, box.x);
-            pos.x = Math.min(pos.x, box.x + box.width - objRect.width);
-            pos.y = Math.max(pos.y, box.y);
-            pos.y = Math.min(pos.y, box.y + box.height - objRect.height);
-            
-            return {
-                x: pos.x,
-                y: pos.y
-            };
-        }
-    });*/
 });
